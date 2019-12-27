da083fa4f9a997fb857418041d00a26729ecf8fe
Stefan Bruggmann <mail@newgen.ch>
2019-12-27 12:52:19 +0100

    BUGFIX: YouTube component

--- components/content/YouTube.vue
+++ components/content/YouTube.vue
@@ -2,14 +2,11 @@
   <figure class="youtube-video">
     <Rowcol align-center small="10" class="offset-top">
       <div class="video-wrapper">
-        <iframe :src="'https://www.youtube.com/embed/'+ getOption('videoId') +'?playsinline=1&autoplay=0&cc_lang_pref='+ lang +'&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&cc_load_policy=1&fs=1&enablejsapi=1'" allowfullscreen allowtransparency allow="autoplay"></iframe>
+        <iframe :src="'https://www.youtube.com/embed/'+ value +'?playsinline=1&autoplay=0&cc_lang_pref='+ lang +'&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&cc_load_policy=1&fs=1&enablejsapi=1'" allowfullscreen allowtransparency allow="autoplay"></iframe>
+        <img :src="image.uri" :alt="image.alt" v-if="image"/>
       </div>
-      <figcaption>
-        <row align-center>
-          <column small="12">
-            <component v-for="contentNode in content" :key="contentNode.identifier" :is="contentNode.type + 'Content'" v-bind="contentNode" />
-          </column>
-        </row>
+      <figcaption v-if="caption">
+        <div v-html="caption"/>
       </figcaption>
     </Rowcol>
   </figure>
@@ -17,7 +14,7 @@
 
 <script>
   export default {
-    props: ['__typename', 'type', 'identifier', 'group', 'options', 'content'],
+    props: ['__typename', 'type', 'identifier', 'group', 'options', 'value', 'caption', 'image'],
 
     computed: {
       lang () {
@@ -42,6 +39,10 @@
         width: 100%;
         height: 100%;
         border: none;
+      }
+
+      img {
+        display: none;
       }
     }
 
