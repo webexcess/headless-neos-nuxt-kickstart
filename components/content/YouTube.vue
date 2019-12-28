bb7409a2855b3370dc8b7e7144e6a0f568761a41
Stefan Bruggmann <mail@newgen.ch>
2019-12-28 23:11:52 +0100

    BUGFIX: Remove wrong row

--- components/content/YouTube.vue
+++ components/content/YouTube.vue
@@ -1,14 +1,12 @@
 <template>
   <figure class="youtube-video">
-    <Rowcol align-center small="10" class="offset-top">
-      <div class="video-wrapper">
-        <iframe :src="'https://www.youtube.com/embed/'+ value +'?playsinline=1&autoplay=0&cc_lang_pref='+ lang +'&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&cc_load_policy=1&fs=1&enablejsapi=1'" allowfullscreen allowtransparency allow="autoplay"></iframe>
-        <img :src="image.uri" :alt="image.alt" v-if="image"/>
-      </div>
-      <figcaption v-if="caption">
-        <div v-html="caption"/>
-      </figcaption>
-    </Rowcol>
+    <div class="video-wrapper">
+      <iframe :src="'https://www.youtube.com/embed/'+ value +'?playsinline=1&autoplay=0&cc_lang_pref='+ lang +'&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&cc_load_policy=1&fs=1&enablejsapi=1'" allowfullscreen allowtransparency allow="autoplay"></iframe>
+      <img :src="image.uri" :alt="image.alt" v-if="image"/>
+    </div>
+    <figcaption v-if="caption">
+      <div v-html="caption"/>
+    </figcaption>
   </figure>
 </template>
 
