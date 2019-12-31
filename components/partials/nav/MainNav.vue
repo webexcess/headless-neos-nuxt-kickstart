156a66ac06162deef925ad6c7cca8b4fc2397c70
Stefan Bruggmann <mail@newgen.ch>
2019-12-31 11:05:37 +0100

    TASK: Allow external link shortcuts

--- components/partials/nav/MainNav.vue
+++ components/partials/nav/MainNav.vue
@@ -5,10 +5,12 @@
         <nuxt-link :to="'/' + dimension">Home</nuxt-link>
       </li>
       <li v-for="node in navigation" :key="node.identifier">
-        <nuxt-link :to="node.uriPath">{{ node.title }}</nuxt-link>
+        <nuxt-link :to="node.uriPath" v-if="!isExternal(node.uriPath)">{{ node.title }}</nuxt-link>
+        <a :href="node.uriPath" target="_blank" v-if="isExternal(node.uriPath)">{{ node.title }}</a>
         <ul v-if="node.subpages && node.subpages.length > 0" class="sub-items">
           <li v-for="subpage in node.subpages" :key="subpage.identifier">
-            <nuxt-link :to="subpage.uriPath">{{ subpage.title }}</nuxt-link>
+            <nuxt-link :to="subpage.uriPath" v-if="!isExternal(subpage.uriPath)">{{ subpage.title }}</nuxt-link>
+            <a :href="subpage.uriPath" target="_blank" v-if="isExternal(subpage.uriPath)">{{ subpage.title }}</a>
           </li>
         </ul>
       </li>
@@ -18,6 +20,7 @@
 
 <script>
   export default {
+
     computed: {
       navigation () {
         return this.$store.getters['navigation/main']
@@ -25,6 +28,12 @@
 
       dimension () {
         return this.$store.state.locale
+      }
+    },
+
+    methods: {
+      isExternal: function (uriPath) {
+        return uriPath.indexOf('http') === 0
       }
     }
   }
